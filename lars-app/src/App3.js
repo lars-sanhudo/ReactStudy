c69198b5b26959034d9c639a2153733a47901f15
lars-sanhudo <sanhudo.lars@gmail.com>
2022-12-21 13:48:43 -0300

    More commit

--- lars-app/src/App3.js
+++ lars-app/src/App3.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Component } from "react";
-
-
+import './app3.css';
 
 class App3 extends Component {
     render() {
         return(
             <div>
-                <h1>First head.</h1>
+                <h1 className="cabecalho">First head.</h1>
                 <h2>Second head.</h2>
                 <p>I'm just a paragraph.</p>
             </div>
